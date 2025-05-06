efd93870e4f35b81b8566ccf46988554ae1e156c
Akila Mendis <godomainz@gmail.com>
2025-05-06 14:35:44 +0530

    175 & 176 update event

--- models/event.go
+++ models/event.go
@@ -70,3 +70,33 @@ func GetEventByID(id int64) (*Event, error){
 	}
 	return &event, nil
 }
+
+func (e Event) Update() error {
+	query := `
+	UPDATE events
+    SET name        = ?,
+        description = ?,
+        location    = ?,
+        dateTime   = ?,
+        user_id     = ?
+    WHERE id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil{
+		return err
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID, e.ID)
+	if err != nil{
+		return err
+	}
+
+	_ , err = result.RowsAffected()
+	if err != nil{
+		return err
+	}
+
+	return nil
+
+}
--- routes/events.go
+++ routes/events.go
@@ -51,3 +51,36 @@ func createEvents(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
 }
+
+func updateEvent(context *gin.Context){
+	
+
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id", "error": err.Error()})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could fetch the event with that event id", "error": err.Error()})
+		return
+	}
+	
+	var event models.Event
+	err = context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse request data.", "error": err.Error()})
+		return
+	}
+
+	event.ID = eventId
+	err = event.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event.", "error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event updated!", "event": event})
+}
