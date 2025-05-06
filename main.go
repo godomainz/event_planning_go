88f98d61618d4b27d4c3fa1562ba3dd3457640ca
Akila Mendis <godomainz@gmail.com>
2025-05-06 13:23:37 +0530

    171. Getting Events From Database (SELECT)

--- main.go
+++ main.go
@@ -18,7 +18,10 @@ func main() {
 }
 
 func getEvents(context *gin.Context){
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil{
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later.", "error": err.Error()})
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -27,13 +30,17 @@ func createEvents(context *gin.Context){
 	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message" : "Could not parse request data.","error":err.Error()})
+		context.JSON(http.StatusInternalServerError, gin.H{"message" : "Could not parse request data.","error": err.Error()})
 		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
-	event.Save()
+	err =  event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message" : "Could not create event.","error": err.Error()})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message" : "event created!", "event": event})
 }
